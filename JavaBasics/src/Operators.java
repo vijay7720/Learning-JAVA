b22a515ac2fda83ba5ddb7543f8a24a8d97ad3ef
vijay <vijay@LAPTOP-8M6183LM>
2021-11-16 23:22:26 +0530

    added operator

--- JavaBasics/src/Operators.java
+++ JavaBasics/src/Operators.java
@@ -3,7 +3,10 @@ public class Operators {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x;
+		int num = 1;
+		num1 += num;
+		
+		System.out.println(num1);
 		
 
 	}
