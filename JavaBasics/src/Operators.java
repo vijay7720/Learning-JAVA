0597fc479786a2cdf6987fbefb2512e37cd61766
vijay <vijay@LAPTOP-8M6183LM>
2021-11-16 23:25:56 +0530

    OperatorTest

--- JavaBasics/src/Operators.java
+++ JavaBasics/src/Operators.java
@@ -1,9 +1,13 @@
 
+
+// Token valid till 16th Dec - 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
+
 public class Operators {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int num = 1;
+		int num1 = 2;
 		num1 += num;
 		
 		System.out.println(num1);
